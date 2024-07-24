43cb960375c6ff3e672f889b11f76c285fd75b40
masso <stefano.massoletti@gmail.com>
2024-07-24 18:09:53 +0200

    First commit

---
+++ 2D_BOWLING/DownBar.gd
@@ -0,0 +1,13 @@
+extends ProgressBar
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	value = get_parent().accumulate_y
+	visible = value >= 10
+	pass
---
+++ 2D_BOWLING/Global.gd
@@ -0,0 +1,4 @@
+extends Node
+
+var accumulator_x = 0.0
+var accumulator_y = 0.0

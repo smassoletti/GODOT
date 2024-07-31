c4fbcd3a8cef9381b41235e8601570afc41d5f6b
smassoletti <stefano.massoletti@gmail.com>
2024-07-31 18:43:03 +0200

    3D - 4.3 - demo shader + barra con forza

--- 3D_BOWLING/Ball/RigidBody3D.gd
+++ 3D_BOWLING/Ball/RigidBody3D.gd
@@ -1,15 +1,42 @@
 extends RigidBody3D
 
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
-var Force = Vector3(0,0,100)
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+const SPEED = 20.0
+var ACCUMULATOR = 0.0
+const SLIDE = 10
 
+var Force_Left = Vector3(SPEED,0,0)
+var Force_Right = Vector3(-SPEED,0,0)
 
 func _physics_process(delta):
 	# Add the gravity.
-	if(Input.is_action_just_pressed("ui_up")):
-		add_constant_force(Force)
+	$"../Sprite3D/SubViewport/ProgressBar".max_value = mass * 2
+	if(Input.is_action_pressed("ui_up")):
+		if ACCUMULATOR < $"../Sprite3D/SubViewport/ProgressBar".max_value:
+			ACCUMULATOR += SPEED*delta
+
+		
+	if(Input.is_action_just_released("ui_up")):
+		if ACCUMULATOR < mass:
+			ACCUMULATOR = 110
+		add_constant_force(Vector3(0,0,ACCUMULATOR*mass))
+		ACCUMULATOR = 0
 		force_update_transform()
+		$"../Sprite3D/SubViewport/ProgressBar".visible = false
+		
+	if(Input.is_action_just_released("ui_down")):
+		add_constant_force(Vector3(0,0,-SPEED))
+		force_update_transform()
+	
+			
+	if(Input.is_action_pressed("ui_left")):
+		position.x += SLIDE*delta
+		$"../Sprite3D".position.x += SLIDE*delta
+	if(Input.is_action_pressed("ui_right")):
+		position.x -= SLIDE*delta
+		$"../Sprite3D".position.x -= SLIDE*delta
+		
+	$"../Sprite3D/SubViewport/ProgressBar".value = ACCUMULATOR
+	
+	
+	

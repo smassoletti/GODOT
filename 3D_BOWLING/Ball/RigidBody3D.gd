af499fab73a6f4ae4bfb03db2a642ee5c68c02d8
smassoletti <stefano.massoletti@gmail.com>
2024-08-01 14:16:07 +0200

    3D - Mini ramp

--- 3D_BOWLING/Ball/RigidBody3D.gd
+++ 3D_BOWLING/Ball/RigidBody3D.gd
@@ -1,7 +1,7 @@
 extends RigidBody3D
 
 
-const SPEED = 20.0
+const SPEED = 80.0
 var ACCUMULATOR = 0.0
 const SLIDE = 10
 
@@ -10,16 +10,20 @@ var Force_Right = Vector3(-SPEED,0,0)
 
 func _physics_process(delta):
 	# Add the gravity.
-	$"../Sprite3D/SubViewport/ProgressBar".max_value = mass * 2
+	$"../Sprite3D/SubViewport/ProgressBar".max_value = 80 * 1.8
 	if(Input.is_action_pressed("ui_up")):
 		if ACCUMULATOR < $"../Sprite3D/SubViewport/ProgressBar".max_value:
 			ACCUMULATOR += SPEED*delta
 
 		
 	if(Input.is_action_just_released("ui_up")):
-		if ACCUMULATOR < mass:
-			ACCUMULATOR = 110
-		add_constant_force(Vector3(0,0,ACCUMULATOR*mass))
+		if ACCUMULATOR < 50:
+			ACCUMULATOR = 50
+		add_constant_force(Vector3(0,0,ACCUMULATOR*mass*1.2))
+		linear_velocity.z += ACCUMULATOR / 2
+		angular_velocity.z += randi_range(-3,3)
+		#angular_velocity.y = randi_range(-1,1)
+		#angular_velocity.x = angular_velocity.z
 		ACCUMULATOR = 0
 		force_update_transform()
 		$"../Sprite3D/SubViewport/ProgressBar".visible = false
